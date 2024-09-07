be8b9d684b026bc170aaabf447fd7d7c4bea6de3
Charlie Spencer <crs992+charr757@gmail.com>
2024-09-07 12:32:52 -0400

    async/await

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -286,7 +286,17 @@ booksAfterUpdate;
 */
 
 // Async Javascript: Promises
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+// console.log("test");
+
+// async js: async/await
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+
+getTodos();
 console.log("test");
